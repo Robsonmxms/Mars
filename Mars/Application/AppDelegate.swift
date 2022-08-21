6b89c93c807492c2122371e777bbdf7b2adb2e24
Robson L. Lopes <robson060814@hotmail.com>
2022-08-20 23:02:35 -0300

    initial layout

--- Mars/Application/AppDelegate.swift
+++ Mars/Application/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Thread.sleep(forTimeInterval: 2.0)
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ Mars/Source/Components/Sizes.swift
@@ -0,0 +1,14 @@
+//
+//  Sizes.swift
+//  Mars
+//
+//  Created by Robson Lima Lopes on 20/08/22.
+//
+
+import Foundation
+import UIKit
+
+struct UIScreenSizes{
+    static let width: CGFloat = UIScreen.main.bounds.width
+    static let height: CGFloat = UIScreen.main.bounds.height
+}
